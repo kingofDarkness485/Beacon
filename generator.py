5755f15a32480d5b76f6186ddbe919722ba7d5c0
kingofDarkness485 <psonone2006@gmail.com>
2025-04-22 23:10:01 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -16,7 +16,7 @@ api_key_cycle = cycle(api_keys)
 @lru_cache(maxsize=50)
 def get_hf_response(question, model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"):
     """Fetch AI-generated responses from Hugging Face API, rotating keys on errors."""
-    api_url = f"https://api-inference.huggingface.co/models/{model_id}"
+    api_url = f"https://api-inference.huggingface.co/models/"
     headers_template = lambda key: {"Authorization": f"Bearer {key}"}
 
     for _ in range(len(api_keys)):
